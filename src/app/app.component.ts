8645ab291a34093cbaf8d0561a9179dca1482698
crishanbr <crishan.0531@gmail.com>
2023-08-25 11:50:15 -0500

    initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'DON KILO SISTEMA DE GESTIÓN';
+}
